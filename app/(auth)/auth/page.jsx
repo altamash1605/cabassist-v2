4c26ada6b9af66f2ab237f0e3aa312a41feeee2d
Altamash <49805368+altamash1605@users.noreply.github.com>
2025-08-25 23:10:38 +0530

    Create page.jsx

---
+++ app/(auth)/auth/page.jsx
@@ -0,0 +1,10 @@
+import AuthCard from "@/components/AuthCard";
+
+export const metadata = {
+  title: "Auth | Login / Sign up"
+};
+
+export default function AuthPage() {
+  return <AuthCard />;
+}
+
